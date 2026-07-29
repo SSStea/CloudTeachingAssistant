b621fa73e3cb954beb3e1a4b9a3b532fedc2c9a3
wangxinbinTesla <1285173093@qq.com>
2026-07-29 14:15:22 +0800

    新增：增加了定时器组件，实现了定时任务添加、删除与超时回调功能，该功能目的是支持网络服务的周期性和延时任务

---
+++ CloudTeachingAssistant/Timer.cpp
@@ -0,0 +1,82 @@
+#include "Timer.h"
+
+CTimer::CTimer(const TimerEvent& event, uint32_t nMsec)
+	: eventCallback(event), m_nInterval(nMsec)
+{
+}
+
+void CTimer::Sleep(uint32_t nMsec)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(nMsec));
+}
+
+void CTimer::SetNextTimeout(uint64_t nTimeNow)
+{
+	m_nNextTimeout = nTimeNow + m_nInterval;
+}
+
+int64_t CTimer::getNextTimeout()
+{
+	return m_nNextTimeout;
+}
+
+TimerId CTimerQueue::AddTimer(const TimerEvent& event, uint32_t nMsec)
+{
+	int64_t nTimePoint = nGetTimeNow(); //获取当前时间
+	TimerId nTimerId = ++nLastTimeId;	//设置id
+
+	auto timer = std::make_shared<CTimer>(event, nMsec); //创建定时任务，智能指针
+
+	timer->SetNextTimeout(nTimePoint);
+	m_mapTimers.emplace(nTimerId, timer);
+	m_mapEvents.emplace(std::pair<int64_t, TimerId>(nTimePoint + nMsec, nTimerId), timer);
+
+	return nTimerId;
+}
+
+void CTimerQueue::RemoveTimer(TimerId nID)
+{
+	auto it = m_mapTimers.find(nID);
+	if (it != m_mapTimers.end())
+	{
+		int64_t nTimeout = it->second->getNextTimeout();
+		m_mapEvents.erase(std::pair<int64_t, TimerId>(nTimeout, nID));
+		m_mapTimers.erase(nID);
+	}
+}
+
+void CTimerQueue::HandleTimerEvent()
+{
+	if (!m_mapTimers.empty())
+	{
+		int64_t nTimePoint = nGetTimeNow(); //获取当前时间
+		while (!m_mapTimers.empty() && 
+			m_mapEvents.begin()->first.first <= nTimePoint)
+			//要处理的这个任务的超时时间不能超过当前时间
+		{
+			TimerId timerId = m_mapEvents.begin()->first.second;
+			//获取这个事件是一次性还是反复执行
+			bool bFlag = m_mapEvents.begin()->second->eventCallback();
+			if (bFlag)
+			{
+				m_mapEvents.begin()->second->SetNextTimeout(nTimePoint);
+				//直接把这个事件移动过来，因为事件是智能指针，避免复制带来的指针加减一
+				auto timerPtr = std::move(m_mapEvents.begin()->second);
+
+				m_mapEvents.erase(m_mapEvents.begin());
+				m_mapEvents.emplace(std::pair<int64_t, TimerId>(timerPtr->getNextTimeout(), timerId), timerPtr);
+			}
+			else
+			{
+				m_mapEvents.erase(m_mapEvents.begin());
+				m_mapTimers.erase(timerId);
+			}
+		}
+	}
+}
+
+int64_t CTimerQueue::nGetTimeNow()
+{
+	auto timeNow = std::chrono::steady_clock::now();
+	return std::chrono::duration_cast<std::chrono::milliseconds>(timeNow.time_since_epoch()).count();
+}
---
+++ CloudTeachingAssistant/Timer.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <map>
+#include <unordered_map>
+#include <cstdint>
+#include <functional>
+#include <chrono>
+#include <memory>
+#include <thread>
+
+//定时实践的回调函数，返回true则是循环执行，false则为一次性事件
+typedef std::function<bool(void)> TimerEvent;
+typedef uint32_t TimerId;
+
+class CTimer//负责定时事件处理
+{
+public:
+	CTimer(const TimerEvent& event, uint32_t nMsec);
+	~CTimer() {}
+
+	static void Sleep(uint32_t nMsec);
+
+private:
+	void SetNextTimeout(uint64_t nTimeNow); //设置下次任务的超时时间
+	int64_t getNextTimeout();   //获取下次任务的超时时间
+
+private:
+	friend class CTimerQueue;
+
+	TimerEvent eventCallback = [] {return false; };//回调函数默认返回false
+
+	uint32_t m_nInterval = 0; //任务的执行时间
+	uint64_t m_nNextTimeout = 0;//下次任务的超时时间
+
+};
+
+
+class CTimerQueue//负责CTimer对象的管理
+{
+public:
+	CTimerQueue() {}
+	~CTimerQueue() {}
+
+public:
+	TimerId AddTimer(const TimerEvent& event, uint32_t nMsec);//添加定时事件
+	void RemoveTimer(TimerId nID);//删除
+	void HandleTimerEvent(); //处理
+
+protected:
+	int64_t nGetTimeNow(); //获取当前时间
+
+private:
+	uint32_t nLastTimeId = 0;//最后一个定时事件的id
+
+	//定时任务表，用定时事件的id排序，存放所有定时事件
+	std::unordered_map<TimerId, std::shared_ptr<CTimer>> m_mapTimers; 
+	//定时事件表，用pair<超时事件，定时事件id>排序，存放所有定时事件
+	std::map<std::pair<int64_t, TimerId>, std::shared_ptr<CTimer>> m_mapEvents;
+};
