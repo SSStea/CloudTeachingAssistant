97c541ede86f13936235d166c523f93992f97f9d
wangxinbinTesla <1285173093@qq.com>
2026-07-29 16:12:13 +0800

    新增：增加了多线程事件循环，实现了Reactor轮询分配、线程启动停止与定时任务转发功能，该功能目的是统一管理服务端事件处理线程

---
+++ CloudTeachingAssistant/EventLoop.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "ReactorEpoll.h"
+#include <vector>
+
+class CEventLoop
+{
+public:
+	CEventLoop(uint32_t nThreadNums = -1);
+	~CEventLoop();
+
+	CEventLoop(const CEventLoop&) = delete;
+	CEventLoop& operator=(const CEventLoop&) = delete;
+
+	std::shared_ptr<CReactorBase> GetReactor();
+	TimerId AddTimer(const TimerEvent& event, uint32_t nMsec);//添加定时器，参数为事件、触发时间
+	void RemoveTimer(TimerId timerId);
+
+	void UpdateChannel(ChannelPtr channel);
+	void RemoveChannel(ChannelPtr& channel);
+
+	void Loop();
+	void Quit();
+
+private:
+	uint32_t m_nThreadNums = -1; //线程数量
+	uint32_t m_nReactorIndex = 1;//reactor索引
+	std::vector<std::shared_ptr<CReactorBase>> m_vecReactors; //存放reactor的vector容器
+	std::vector<std::shared_ptr<std::thread>> m_vecThreads; //存放线程
+};
+
