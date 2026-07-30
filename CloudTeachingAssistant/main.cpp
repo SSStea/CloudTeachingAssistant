78a25deae38d0abaff7be4a6b5322f0df1c282eb
wangxinbinTesla <1285173093@qq.com>
2026-07-30 14:07:13 +0800

    修改：将示例程序入口修改为启动TCP服务端并等待退出，修改的原因为需要运行和验证事件循环网络服务，解决了工程仅输出占位文本无法启动服务bug

--- CloudTeachingAssistant/main.cpp
+++ CloudTeachingAssistant/main.cpp
@@ -1,7 +1,20 @@
-﻿#include <iostream>
+#include <iostream>
+#include "EventLoop.h"
+#include "TcpServer.h"
 
 int main()
 {
-    std::cout << "hello edoyun" << std::endl;
+    uint32_t nCnt = std::thread::hardware_concurrency();
+    CEventLoop eventLoop(nCnt);
+    CTcpServer* server = new CTcpServer(&eventLoop);
+
+    server->bStart("172.20.108.206", 9527);
+    std::cout << "server start" << std::endl;
+
+    getchar();
+
+    server->Stop();
+
+    std::cout << "server terminal" << std::endl;
     return 0;
 }
