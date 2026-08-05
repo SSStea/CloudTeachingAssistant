75cb4425f7877b8a52d87a50e399876673f64dc1
wangxinbinTesla <1285173093@qq.com>
2026-08-05 11:35:47 +0800

    新增：增加了完整CRtmpConnection类，实现了TCP读写回调、RTMP握手与分块处理、消息类型分派、流量控制消息发送及工程登记功能，该功能目的是衔接TCP连接与RTMP协议会话处理

---
+++ CloudTeachingAssistant/RtmpConnection.cpp
@@ -0,0 +1,244 @@
+#include "RtmpConnection.h"
+
+CRtmpConnection::CRtmpConnection(CReactorBase* reactor, int nSocket)
+	: CTcpConnection(reactor, nSocket), m_state(HANDSHAKE),
+	m_pRtmpChunk(new CRtmpChunk())
+{
+	//设置回调函数，处理读数据
+	this->setReadCallback([this](std::shared_ptr<CTcpConnection> conn, CBufferReader& buffer)
+		{
+			return this->bOnRead(buffer);
+		});
+	
+	//设置关闭回调，释放资源
+	this->setCloseCallback([this](std::shared_ptr<CTcpConnection> conn)
+		{
+			this->OnClose();
+		});
+
+}
+
+CRtmpConnection::~CRtmpConnection()
+{
+}
+
+bool CRtmpConnection::bOnRead(CBufferReader& buffer)
+{
+	bool bRet = true;
+
+	if (m_pHandShake->bHandshakeIsCompleted())//是否rtmp握手完成，完成之后才能发送message
+	{
+		bRet = bHandleChunk(buffer);
+	}
+	else
+	{
+		//分配内存，存储解析握手的结果
+		std::shared_ptr<char> res(new char[4096], std::default_delete<char[]>());
+		//解析握手
+		int nResSize = m_pHandShake->nParse(buffer, res.get(), 4096);
+		if (nResSize < 0)
+		{
+			bRet = false;
+		}
+		if(nResSize > 0)
+		{
+			//发送握手结果
+			this->Send(res.get(), nResSize);
+		}
+
+		if (m_pHandShake->bHandshakeIsCompleted() && buffer.nReadableBytes() > 0)
+		{
+			bRet = bHandleChunk(buffer);
+		}
+
+	}
+
+	return bRet;
+}
+
+void CRtmpConnection::OnClose()
+{
+	this->bHandleDeleteStream();
+}
+
+bool CRtmpConnection::bHandleChunk(CBufferReader& buffer)
+{
+	int nRet = -1;
+
+	do 
+	{
+		RtmpMessage msg;
+		nRet = m_pRtmpChunk->nParse(buffer, msg);
+		if (nRet >= 0)
+		{
+			if (msg.bIsCompleted())
+			{
+				if (!bHandleMessage(msg))
+				{
+					std::cout << "hanlde rtmp message fail" << std::endl;
+					return false;
+				}
+			}
+			if (nRet == 0)
+			{
+				break;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	} while (buffer.nReadableBytes() > 0);
+
+	return true;
+}
+
+bool CRtmpConnection::bHandleMessage(RtmpMessage& rtmpMsg)
+{
+	bool bRet = true;
+
+	switch (rtmpMsg.nTypeId)
+	{
+	case RTMP_VIDEO:
+		bRet = bHandleVideo(rtmpMsg);
+		break;
+
+	case  RTMP_AUDIO:
+		bRet = bHandleAudio(rtmpMsg);
+		break;
+
+	case RTMP_INVOKE:
+		bRet = bHandleInvoke(rtmpMsg);
+		break;
+
+	case RTMP_NOTIFY:
+		bRet = bHandleNotify(rtmpMsg);
+		break;
+
+	case RTMP_SET_CHUNK_SIZE:
+		m_pRtmpChunk->SetInChunkSize(ReadUint32BE(rtmpMsg.pPlayload.get()));
+		break;
+
+	default:
+		break;
+	}
+
+	return bRet;
+}
+
+bool CRtmpConnection::bHandleInvoke(RtmpMessage& rtmpMsg)
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandleNotify(RtmpMessage& rtmpMsg)
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandleAudio(RtmpMessage& rtmpMsg)
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandleVideo(RtmpMessage& rtmpMsg)
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandleConnection()
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandleCreateStream()
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandlePublish()
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandlePlay()
+{
+	return false;
+}
+
+bool CRtmpConnection::bHandleDeleteStream()
+{
+	return false;
+}
+
+void CRtmpConnection::SetPeerBandWidth()
+{
+	std::shared_ptr<char> pData(new char[5], std::default_delete<char[]>());
+
+	WriteUint32BE(pData.get(), m_nPeerWidth);
+	pData.get()[4] = 2; //0：客户端必须按带宽处理，超出的话丢弃；1：软限制，可以按带宽处理允许超出
+						//2：动态限制，表示客户端可以按照网络限制动态调整
+	RtmpMessage msg;
+	msg.nTypeId = RTMP_BANDWIDTH_SIZE;
+	msg.pPlayload = pData;
+	msg.nLength = 5;
+	this->SendRtmpChunks(RTMP_CHUNK_CONTROL_ID, msg);
+}
+
+void CRtmpConnection::SendAcknowlegement()
+{
+	std::shared_ptr<char> pData(new char[4], std::default_delete<char[]>());
+	WriteUint32BE(pData.get(), m_nAcknowledgementSize);
+
+	RtmpMessage msg;
+	msg.nTypeId = RTMP_ACK_SIZE;
+	msg.pPlayload = pData;
+	msg.nLength = 4;
+	this->SendRtmpChunks(RTMP_CHUNK_CONTROL_ID, msg);
+}
+
+void CRtmpConnection::SetChunkSize()
+{
+	m_pRtmpChunk->SetOutChunkSize(m_nMaxChunkSize);
+	std::shared_ptr<char> pData(new char[4], std::default_delete<char[]>());
+	WriteUint32BE(pData.get(), m_nMaxChunkSize);
+
+	RtmpMessage msg;
+	msg.nTypeId = RTMP_SET_CHUNK_SIZE;
+	msg.pPlayload = pData;
+	msg.nLength = 4;
+	this->SendRtmpChunks(RTMP_CHUNK_CONTROL_ID, msg);
+}
+
+bool CRtmpConnection::bSendInvokeMessage(uint32_t nCsId, std::shared_ptr<char> pPayload, uint32_t nPayloadSize)
+{
+	return false;
+}
+
+bool CRtmpConnection::bSendNotifyMessage(uint32_t nCsId, std::shared_ptr<char> pPayload, uint32_t nPayloadSize)
+{
+	return false;
+}
+
+void CRtmpConnection::SendRtmpChunks(uint32_t nCsId, RtmpMessage& rtmpMsg)
+{
+	//创建块
+	uint32_t nCapacity = rtmpMsg.nLength + rtmpMsg.nLength / m_nMaxChunkSize * 5 + 1024;
+	std::shared_ptr<char> pBuffer(new char[nCapacity], std::default_delete<char[]>());
+	int nSize = m_pRtmpChunk->nCreateChunk(nCsId, rtmpMsg, pBuffer.get(), nCapacity);
+
+	if (nSize > 0)
+	{
+		this->Send(pBuffer.get(), nSize);
+	}
+}
+
+bool CRtmpConnection::bSendMetaData(mapAmfObjects metaData)
+{
+	return false;
+}
+
+bool CRtmpConnection::bSendMediaData(uint8_t nType, uint64_t nTimeStamp, std::shared_ptr<char> pPlayload, uint32_t nPlayloadSize)
+{
+	return false;
+}
---
+++ CloudTeachingAssistant/RtmpConnection.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "TcpConnection.h"
+#include "Amf.h"
+#include "RtmpSink.h"
+#include "RtmpChunk.h"
+#include "RtmpHandshake.h"
+#include "Rtmp.h"
+
+class CRtmpConnection : public CTcpConnection, public CRtmpSink
+{
+public:
+	enum ConnectionState
+	{
+		HANDSHAKE,
+		START_CONNECT,
+		START_CREATE_STREAM,
+		START_DELETE_STREAM,
+		START_PLAY,
+		START_PUBLISH,
+	};
+
+	CRtmpConnection(CReactorBase* reactor, int nSocket);
+	virtual ~CRtmpConnection();
+
+
+	virtual bool bIsPlayer() override { return m_state == START_PLAY; }
+	virtual bool bIsPublisher() override { return m_state == START_PUBLISH; }
+	virtual bool bIsPlaying() override { return m_bIsPlaying; }
+	virtual bool bIsPublishing() override { return m_bIsPublishing; }
+
+	virtual uint32_t nGetId() override { return this->nGetSocket(); }
+
+private:
+	bool bOnRead(CBufferReader& buffer);//读数据
+	void OnClose();
+
+	bool bHandleChunk(CBufferReader& buffer);//处理块
+	bool bHandleMessage(RtmpMessage& rtmpMsg);//处理消息
+	bool bHandleInvoke(RtmpMessage& rtmpMsg);//处理调用
+	bool bHandleNotify(RtmpMessage& rtmpMsg);//处理通知
+	bool bHandleAudio(RtmpMessage& rtmpMsg);//处理音频
+	bool bHandleVideo(RtmpMessage& rtmpMsg);//处理视频
+	bool bHandleConnection();//处理rtmp连接
+	bool bHandleCreateStream();//处理创建流
+	bool bHandlePublish();//处理推流
+	bool bHandlePlay();// 处理拉流
+	bool bHandleDeleteStream();//处理删除流
+
+	void SetPeerBandWidth();//设置带宽
+	void SendAcknowlegement();//发送接收大小
+	void SetChunkSize();//设置块大小
+
+	//发送启动消息
+	bool bSendInvokeMessage(uint32_t nCsId, std::shared_ptr<char> pPayload, uint32_t nPayloadSize);
+	//发送通知消息
+	bool bSendNotifyMessage(uint32_t nCsId, std::shared_ptr<char> pPayload, uint32_t nPayloadSize);
+	void SendRtmpChunks(uint32_t nCsId, RtmpMessage& rtmpMsg);//发送块数据
+
+	virtual bool bSendMetaData(mapAmfObjects metaData) override;
+	virtual bool bSendMediaData(uint8_t nType, uint64_t nTimeStamp, std::shared_ptr<char> pPlayload, uint32_t nPlayloadSize)override;
+
+private:
+	ConnectionState m_state;
+
+	std::shared_ptr<CRtmpHandshake> m_pHandShake;
+	std::shared_ptr<CRtmpChunk> m_pRtmpChunk;
+
+	uint32_t m_nPeerWidth;
+	uint32_t m_nAcknowledgementSize;
+	uint32_t m_nMaxChunkSize;
+	uint32_t m_nStreamID;
+
+	mapAmfObjects m_amfobjMetaData;
+	CAmfDecoder m_amfDecoder;
+	CAmfEncoder m_amfEncoder;
+
+	bool m_bIsPlaying = false;
+	bool m_bIsPublishing = false;
+
+	//元数据
+	std::shared_ptr<char> m_pAvcSequenceHeader;
+	std::shared_ptr<char> m_pAacSequenceHeader;
+	uint32_t m_nAvcSequenceHeaderSize = 0;
+	uint32_t m_nAacSequenceHeaderSize = 0;
+
+};
+
