778d346b4ccb7cae59c70b239603e355dadd4af5
wangxinbinTesla <1285173093@qq.com>
2026-08-12 22:22:23 +0800

    修改：主程序切换为负载均衡与登录服务联动启动
    1、将信令服务头文件及SigeServerTest启动流程停用；
    2、增加负载均衡服务与登录服务依赖，并在同一事件循环中依次启动8523与9867端口；
    3、将main入口修改为执行LoginTest登录服务启动流程；
    修改的原因为当前运行目标已由信令服务切换为登录服务及其负载均衡节点；
    解决了主程序无法启动和联调登录服务链路bug

--- CloudTeachingAssistant/main.cpp
+++ CloudTeachingAssistant/main.cpp
@@ -2,7 +2,9 @@
 #include "EventLoop.h"
 #include "TcpServer.h"
 #include "RtmpServer.h"
-#include "SigServer.h"
+//#include "SigServer.h"
+#include "LoginServer.h"
+#include "LoadBalanceServer.h"
 
 void TcpTest()
 {
@@ -40,24 +42,46 @@ void RtmpSeverTest()
 	getchar();
 }
 
-void SigeServerTest()
+//void SigeServerTest()
+//{
+//	uint32_t nCnt = std::thread::hardware_concurrency();
+//	CEventLoop eventLoop(nCnt);
+//	auto sigServer = CSigServer::pCreate(&eventLoop);
+//	if (sigServer->bStart("172.20.108.206", 6539))
+//	{
+//		std::cout << "sig server start success" << std::endl;
+//	}
+//	else
+//	{
+//		std::cout << "sig server start fail" << std::endl;
+//	}
+//	getchar();
+//}
+
+void LoginTest()
 {
 	uint32_t nCnt = std::thread::hardware_concurrency();
+	std::shared_ptr<CLoginServer> loginServer = nullptr;
 	CEventLoop eventLoop(nCnt);
-	auto sigServer = CSigServer::pCreate(&eventLoop);
-	if (sigServer->bStart("172.20.108.206", 6539))
+	auto loadServer = CLoadBalanceServer::pCreate(&eventLoop);
+
+	if (loadServer->bStart("172.20.108.206", 8523))
 	{
-		std::cout << "sig server start success" << std::endl;
-	}
-	else
-	{
-		std::cout << "sig server start fail" << std::endl;
+		loginServer = CLoginServer::pCreate(&eventLoop);
+		if (loginServer->bStart("172.20.108.206", 9867))
+		{
+			std::cout << "server start success\n";
+		}
+		else
+		{
+			std::cout << "server start fail\n";
+		}
 	}
 	getchar();
 }
 
 int main()
 {
-	SigeServerTest();
+	LoginTest();
     return 0;
 }
