5780b869038b6672efbbddb29e481c64fa3fa91b
wangxinbinTesla <1285173093@qq.com>
2026-08-19 16:35:55 +0800

    新增：完整CTimerQueue定时任务队列类
    1、增加了定时任务编号分配及按编号保存的任务映射；
    2、实现了定时任务添加、删除和按超时时间排序；
    3、实现了到期任务回调执行、循环任务重新排期及一次性任务清理；
    该功能目的是集中管理多个网络定时事件，并按触发时间顺序执行回调

--- ECloudAssistant/Net/Timer.h
+++ ECloudAssistant/Net/Timer.h
@@ -32,3 +32,27 @@ private:
 	uint64_t m_nNextTimeout = 0;//下次任务的超时时间
 
 };
+
+
+class CTimerQueue//负责CTimer对象的管理
+{
+public:
+	CTimerQueue() {}
+	~CTimerQueue() {}
+
+public:
+	TimerId AddTimer(const TimerEvent& event, uint32_t nMsec);//添加定时事件
+	void RemoveTimer(TimerId nID);//删除
+	void HandleTimerEvent(); //处理
+
+protected:
+	int64_t nGetTimeNow(); //获取当前时间
+
+private:
+	uint32_t nLastTimeId = 0;//最后一个定时事件的id
+
+	//定时任务表，用定时事件的id排序，存放所有定时事件
+	std::unordered_map<TimerId, std::shared_ptr<CTimer>> m_mapTimers;
+	//定时事件表，用pair<超时事件，定时事件id>排序，存放所有定时事件
+	std::map<std::pair<int64_t, TimerId>, std::shared_ptr<CTimer>> m_mapEvents;
+};
