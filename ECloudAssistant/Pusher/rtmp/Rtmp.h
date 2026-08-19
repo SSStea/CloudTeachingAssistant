b501b223ff0580c86b0294c07d33e371cb692dfa
wangxinbinTesla <1285173093@qq.com>
2026-08-19 16:37:42 +0800

    新增：完整CRtmp协议会话基础类
    1、增加了Chunk大小、确认窗口和对端带宽设置及查询；
    2、实现了带端口和默认1935端口两种RTMP URL解析；
    3、实现了服务器地址、应用名、流名称和完整流路径提取与保存；
    该功能目的是为RTMP连接和发布器提供统一会话参数、流地址解析和传输窗口配置基础

--- ECloudAssistant/Pusher/rtmp/Rtmp.h
+++ ECloudAssistant/Pusher/rtmp/Rtmp.h
@@ -59,3 +59,112 @@ struct RtmpMediaInfo
     std::shared_ptr<quint8> m_pAudioSpecificConfig;//AAC 配置信息
     quint32 m_nAudioSpecificConfigSize = 0;//AAC 配置信息大小
 };
+
+
+class CRtmp
+{
+public:
+	virtual ~CRtmp() {};
+
+	void SetChunkSize(uint32_t nSize)
+	{
+		if (nSize > 0 && nSize <= 60000) {
+			m_nMaxChunkSize = nSize;
+		}
+	}
+
+	void SetPeerBandwidth(uint32_t nSize)
+	{
+		m_nPeerBandwidth = nSize;
+	}
+
+	uint32_t nGetChunkSize() const
+	{
+		return m_nMaxChunkSize;
+	}
+
+	uint32_t nGetAcknowledgementSize() const
+	{
+		return m_nAcknowledgementSize;
+	}
+
+	uint32_t nGetPeerBandwidth() const
+	{
+		return m_nPeerBandwidth;
+	}
+
+	virtual int nParseRtmpUrl(std::string strUrl)
+	{
+		char chIP[100] = { 0 };
+		char chStreamPath[500] = { 0 };
+		char chApp[100] = { 0 };
+		char chStreamName[400] = { 0 };
+		uint16_t nPort = 0;
+
+		if (strstr(strUrl.c_str(), "rtmp://") == nullptr) {
+			return -1;
+		}
+
+#if defined(__linux) || defined(__linux__)
+		if (sscanf(strUrl.c_str() + 7, "%[^:]:%hu/%s", chIP, &nPort, chStreamPath) == 3)
+#elif defined(WIN32) || defined(_WIN32)
+        if (sscanf_s(strUrl.c_str() + 7, "%[^:]:%hu/%s", chIP, 100, &nPort, chStreamPath, 500) == 3)
+#endif
+		{
+			m_nPort = nPort;
+		}
+#if defined(__linux) || defined(__linux__)
+		else if (sscanf(strUrl.c_str() + 7, "%[^/]/%s", chIP, chStreamPath) == 2)
+#elif defined(WIN32) || defined(_WIN32)
+        else if (sscanf_s(strUrl.c_str() + 7, "%[^/]/%s", chIP, 100, chStreamPath, 500) == 2)
+#endif
+		{
+			m_nPort = 1935;
+		}
+		else {
+			return -1;
+		}
+
+		m_strIP = chIP;
+		m_strStreamPath += "/";
+		m_strStreamPath += chStreamPath;
+
+#if defined(__linux) || defined(__linux__)
+		if (sscanf(m_strStreamPath.c_str(), "/%[^/]/%s", chApp, chStreamName) != 2)
+#elif defined(WIN32) || defined(_WIN32)
+        if (sscanf_s(m_strStreamPath.c_str(), "/%[^/]/%s", chApp, 100, chStreamName, 400) != 2)
+#endif
+		{
+			return -1;
+		}
+
+		m_strApp = chApp;
+		m_strStreamName = chStreamName;
+		return 0;
+	}
+
+	std::string strGetStreamPath() const
+	{
+		return m_strStreamPath;
+	}
+
+	std::string strGetApp() const
+	{
+		return m_strApp;
+	}
+
+	std::string strGetStreamName() const
+	{
+		return m_strStreamName;
+	}
+
+	uint16_t m_nPort = 1935;
+	std::string m_strIP;
+	std::string m_strApp;
+	std::string m_strStreamName;
+	std::string m_strStreamPath;
+
+	uint32_t m_nPeerBandwidth = 5000000;
+	uint32_t m_nAcknowledgementSize = 5000000;
+	uint32_t m_nMaxChunkSize = 128;
+};
